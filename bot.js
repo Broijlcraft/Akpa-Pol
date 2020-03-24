e542fc7c04c0b960070ca87505328967aa7176ad
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-03-24 19:40:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message',message => {
      if (message.author.bot) return;
      if (message.content.toLowerCase().includes('ping')){
          message.reply('Pong!');
-        client.channels.get('692070737900470323').send('🕗');
+         client.channels.get('692070737900470323').send('🕗');
      }
 });
 
