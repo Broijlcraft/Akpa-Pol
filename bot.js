14e460b802919001362094dd5e0ecb83176dbaef
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-03-24 20:41:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,15 @@ var client = new discord.Client();
 
 client.on('ready', ()=>{
     console.log('online');
+    currentDate = new Date();    
+    client.channels.get('692070737900470323').send(currentDate.getDate() + '-' + currentDate.getHours() + '-' + currentDate.getMinutes());
 });
 
 client.on('message',message => {
      if (message.author.bot) return;
      if (message.content.toLowerCase().includes('ping')){
          message.reply('Pong!');
-         client.channels.get('692070737900470323').send('🕗');
-         client.channels.get('692070737900470323').send('🕔');
+         //client.channels.get('692070737900470323').send('🕔');
      }
 });
 
