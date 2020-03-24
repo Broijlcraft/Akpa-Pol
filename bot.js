eeb3ed7c7128c7eb1b780537a04ae2cd0d83c0a7
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-03-24 19:28:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const discord = require('discord.js');
 var client = new discord.Client();
 
-const token = 'NjkxNjUzMzI4MDAxNjk1NzQ0.XnjGbg.i79JcvEOHxc9aBTmNsH8N94QnqA';
-
 client.on('ready', ()=>{
     console.log('online');
     client.channels.get('692070737900470323').send('@everyone de les begint');
