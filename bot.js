84bf5522bd57281f6c88b6615e696e4b770d47fb
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-03-25 09:18:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,8 +56,8 @@ client.on('messageReactionAdd', (reaction, user)=>{
     var hour_ = currentDate.getHours();
     var minute_ = currentDate.getMinutes();
     hour_ = hour_ + 7;
-    //client.channels.get('692133614363738184').send(user.username + " reacted with " + reaction.emoji.name + " at " + hour_ +':'+ minute_);
-    client.channels.get('692070737900470323').send(user.username + " reacted with " + reaction.emoji.name + " at " + hour_ +':'+ minute_);
+    client.channels.get('692133614363738184').send(user.username + " reacted with " + reaction.emoji.name + " at " + hour_ +':'+ minute_);
+    //client.channels.get('692070737900470323').send(user.username + " reacted with " + reaction.emoji.name + " at " + hour_ +':'+ minute_);
 })
 
 //client.on("message", message => {
