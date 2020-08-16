99c68358905081686390d846c2d92821b7b1ad29
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-08-16 23:08:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const discord = require('discord.js');
 var client = new discord.Client();
 
-client.on('ready', ()=>{       
+client.on('ready', ()=>{
+    if(1!=2)return;
     setInterval(function() {
         currentDate = new Date();
         var day_ = currentDate.getDay();
@@ -55,6 +56,7 @@ client.on('ready', ()=>{
 });
 
 client.on('messageReactionAdd', (reaction, user)=>{
+    if(1!=2)return;
     if(user.bot)return 
     if(reaction.message.channel.id === process.env.SURVEY_CHANNEL) {
         currentDate = new Date();
@@ -78,6 +80,7 @@ client.on('messageReactionAdd', (reaction, user)=>{
 })
 
 client.on('messageReactionAdd', (reaction, user)=>{
+    if(1!=2)return;
     if(user.bot)return 
     if(reaction.message.channel.id === process.env.BOT_TEST_SURVEY_CHANNEL) {
         currentDate = new Date();
@@ -94,6 +97,7 @@ client.on('messageReactionAdd', (reaction, user)=>{
 })
 
 client.on("message", message => {
+    if(1!=2)return;
     if(message.author.bot){return}
         if(message.channel.id === process.env.BOT_TEST_SURVEY_CHANNEL){
             if(message.content.toLocaleLowerCase().includes('test')){
