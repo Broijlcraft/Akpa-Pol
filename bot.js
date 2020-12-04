ff79398e751bb3d97c9f1db69c6e35af57d278c2
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-12-04 10:17:28 +0100

    Bot Cleanup and removing test channel

--- bot.js
+++ bot.js
@@ -1,115 +1,86 @@
-// const discord = require('discord.js');
-// var client = new discord.Client();
+const discord = require('discord.js');
+var client = new discord.Client();
 
-// client.on('ready', ()=>{
-//     if(1!==2)return;
-//     setInterval(function() {
-//         currentDate = new Date();
-//         var day_ = currentDate.getDay();
-//         var hour_ = currentDate.getHours();
-//         var minute_ = currentDate.getMinutes();
-//         if(day_ > 0){
-//             if(day_ < 6) {
-//                 if(hour_ === 0 && minute_ === 1){
-//                     client.channels.get(process.env.SURVEY_CHANNEL).send('@everyone');
-//                         client.channels.get(process.env.SURVEY_CHANNEL).send({embed: {
-//                             color: 15844367,
-//                             title: "Click emoji pada waktu Check-in. Paling cepat 15 menit sebelum Jam kerja:",
-//                                 fields: [
-//                                     { name: "Click reaction", value: "🅰️    Untuk AKPA", inline: true},
-//                                     { name: "Click reaction", value: "🇵    Untuk Polaris ", inline: true}
-//                                 ]
-//                             }
-//                         }).then(sentEmbed => {
-//                             sentEmbed.react('🅰️'),
-//                             sentEmbed.react('🇵')
-//                         });            
-//                         client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
-//                             color: 15844367,
-//                             title: "Check-in:"
-//                             }
-//                         })
-//                     }    
-//                     if(hour_ === 9 && minute_ === 58){
-//                         client.channels.get(process.env.SURVEY_CHANNEL).send('@everyone');
-//                         client.channels.get(process.env.SURVEY_CHANNEL).send({embed: {
-//                             color: 15844367,
-//                             title: "Click emoji pada waktu Check-out:",
-//                                 fields: [
-//                                     { name: "Click reaction", value: "🅰️   Untuk AKPA", inline: true},
-//                                     { name: "Click reaction", value: "🇵    Untuk Polaris ", inline: true}
-//                                 ]
-//                             }
-//                         }).then(sentEmbed => {
-//                             sentEmbed.react('🅰️'),
-//                             sentEmbed.react('🇵')
-//                         });
-//                         client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
-//                             color: 15844367,
-//                             title: "Check-out:"
-//                         }
-//                     })
-//                 } 
-//             }
-//         }
-//     }, 60000);
-// });
+//process.env variables are available through the Heroku control panel
 
-// client.on('messageReactionAdd', (reaction, user)=>{
-//     if(1!==2)return;
-//     if(user.bot)return 
-//     if(reaction.message.channel.id === process.env.SURVEY_CHANNEL) {
-//         currentDate = new Date();
-//         var day_ = currentDate.getDay();
-//         var hour_ = currentDate.getHours();
-//         var minute_ = currentDate.getMinutes();
-//         if(day_ > 0){
-//             if(day_ < 6){
-//                 hour_ = hour_ + 7;
-//                 client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
-//                     color: 15844367,
-//                     title: "Employee:",
-//                     fields: [
-//                             { name: "Info", value: "Name: " + user.username + "\nReaction: " + reaction.emoji.name + "\nTime: " + hour_ + ':' + minute_ , inline: false},
-//                         ]
-//                     }
-//                 });
-//             }
-//         }
-//     }
-// })
+client.on('ready', ()=>{
+    if(1!==2)return;
+    setInterval(function() {
+        currentDate = new Date();
+        var day_ = currentDate.getDay();
+        var hour_ = currentDate.getHours();
+        var minute_ = currentDate.getMinutes();
+        if(day_ > 0){
+            if(day_ < 6) {
+                if(hour_ === 0 && minute_ === 1){
+                    client.channels.get(process.env.SURVEY_CHANNEL).send('@everyone');
+                        client.channels.get(process.env.SURVEY_CHANNEL).send({embed: {
+                            color: 15844367,
+                            title: process.env.TITLE_CHECKIN,
+                                fields: [
+                                    //OP = Option
+                                    { name: process.env.OP_NAME_CHECKIN_1, value: process.env.OP_VALUE_CHECKIN_1, inline: true},
+                                    { name: process.env.OP_NAME_CHECKIN_2, value: process.env.OP_VALUE_CHECKIN_2, inline: true}
+                                ]
+                            }
+                        }).then(sentEmbed => {
+                            // EMT = discord emote, copy from discord using \:smile: in chat to get 😄
+                            sentEmbed.react(process.env.OP_EMT_CHECKIN_1),
+                            sentEmbed.react(process.env.OP_EMT_CHECKIN_2)
+                        });            
+                        client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
+                            color: 15844367,
+                            title: process.env.TITLE_CHECKIN_RESULTS
+                            }
+                        })
+                    }    
+                    if(hour_ === 9 && minute_ === 58){
+                        client.channels.get(process.env.SURVEY_CHANNEL).send('@everyone');
+                        client.channels.get(process.env.SURVEY_CHANNEL).send({embed: {
+                            color: 15844367,
+                            title: process.env.TITLE_CHECKOUT,
+                                fields: [
+                                    { name: process.env.OP_NAME_CHECKOUT_1, value: process.env.OP_VALUE_CHECKOUT_1, inline: true},
+                                    { name: process.env.OP_NAME_CHECKOUT_2, value: process.env.OP_VALUE_CHECKOUT_2, inline: true}
+                                ]
+                            }
+                        }).then(sentEmbed => {
+                            sentEmbed.react(process.env.OP_EMT_CHECKOUT_1),
+                            sentEmbed.react(process.env.OP_EMT_CHECKOUT_2)
+                        });
+                        client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
+                            color: 15844367,
+                            title: process.env.TITLE_CHECKOUT_RESULTS
+                        }
+                    })
+                } 
+            }
+        }
+    }, 60000);
+});
 
-// client.on('messageReactionAdd', (reaction, user)=>{
-//     if(1!==2)return;
-//     if(user.bot)return 
-//     if(reaction.message.channel.id === process.env.BOT_TEST_SURVEY_CHANNEL) {
-//         currentDate = new Date();
-//         var day_ = currentDate.getDay();
-//         var hour_ = currentDate.getHours();
-//         var minute_ = currentDate.getMinutes();
-//         hour_ = hour_ + 7;
-//         client.channels.get(process.env.BOT_TEST_RESULTS_CHANNEL).send({embed: {
-//             color: 15844367,
-//             title: "Employee:",
-//             fields: [{ name: "Info", value: "Name: " + user.username + "\nReaction: " + reaction.emoji.name + "\nTime: " + hour_ + ':' + minute_ , inline: false}]}
-//         });
-//     }
-// })
+client.on('messageReactionAdd', (reaction, user)=>{
+    if(1!==2)return;
+    if(user.bot)return 
+    if(reaction.message.channel.id === process.env.SURVEY_CHANNEL) {
+        currentDate = new Date();
+        var day_ = currentDate.getDay();
+        var hour_ = currentDate.getHours();
+        var minute_ = currentDate.getMinutes();
+        if(day_ > 0){
+            if(day_ < 6){
+                hour_ = hour_ + 7;
+                client.channels.get(process.env.RESULTS_CHANNEL).send({embed: {
+                    color: 15844367,
+                    title: process.env.TITLE_ROLE_DESCRIPT,
+                    fields: [
+                            { name: process.env.RESP_NAME, value: process.env.RESP_USERPREFX + user.username + process.env.RESP_REACTPREFX  + reaction.emoji.name + process.env.RESP_TIMEPREFX + hour_ + ':' + minute_ , inline: false},
+                        ]
+                    }
+                });
+            }
+        }
+    }
+})
 
-// client.on("message", message => 
-//     if(1!==2)return
-//     if(message.author.bot){return}
-//         if(message.channel.id === process.env.BOT_TEST_SURVEY_CHANNEL){
-//             if(message.content.toLocaleLowerCase().includes('test')){
-//                 //client.channels.get(process.env.BOT_TEST_SURVEY_CHANNEL).send('@everyone');
-//                 client.channels.get(process.env.BOT_TEST_SURVEY_CHANNEL).send({embed: {
-//                     color: 15844367,
-//                     title: "Button test",
-//                     fields: [{ name: "Click reaction", value: "🧪   Untuk AKPA", inline: true}]}})
-//                     .then(sentEmbed => {sentEmbed.react('🧪')});   
-//             };
-//         }
-//     }
-// )
-
-// client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
